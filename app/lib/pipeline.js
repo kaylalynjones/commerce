144204c7dcc4a9c17bd027326b1c3a7c6ae89ac1
Kayla Jones <KaylaLyne424@gmail.com>
2014-08-05 10:34:35 -0500

    tweaks

--- app/lib/pipeline.js
+++ app/lib/pipeline.js
@@ -3,13 +3,14 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var home = require('../controllers/home');
+var methodOverride = require('express-method-override');
 var items = require('../controllers/items');
 
 module.exports = function(app, express) {
   app.use(morgan('dev'));
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
-
+  app.use(methodOverride());
   app.get('/', home.index);
   app.get('/about', home.about);
   app.get('/faq', home.faq);
@@ -18,7 +19,7 @@ module.exports = function(app, express) {
   app.post('/items', items.create);
   app.get('/items', items.index);
   app.get('/items/:id', items.show);
-  app.post('/items/:id/delete', items.destroy);
+  app.delete('/items/:id', items.destroy);
 
   console.log('Pipeline configured');
 };
